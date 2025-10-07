62a61a60e6e683f4b24c7e1c6c5ecdf035ea13f3
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2025-10-06 19:53:01 -0700

    added component implementation to app.js

--- src/app.js
+++ src/app.js
@@ -2,43 +2,49 @@ import * as guiHTML from './screens/gui.html'
 
 import './styles/main.scss'
 import './styles/share.scss'
+import './styles/project.scss'
 
 // import { ScreenshotShare } from './components/screenshot-share'
 // import { VideoWatermarkShare } from './components/video-share'
 // import { MainFunctions } from './components/functions'
 import { PortfolioGenerate } from './components/portfolio-generate'
-
+import { ProjectGenerate } from './components/project-generate'
 
 document.body.style.touchAction = 'none'
 document.body.insertAdjacentHTML('afterbegin', guiHTML)
 
-
 const components = {
-    'portfolio-generate': PortfolioGenerate
+  'portfolio-generate': PortfolioGenerate,
+  'project-generate': ProjectGenerate,
 }
 
 function registerComponent(name, definition) {
-  components[name] = definition;
+  components[name] = definition
 
   // Initialize existing elements with this attribute
-  document.querySelectorAll(`[${name}]`).forEach(el => {
-    definition.init?.call(el, el.getAttribute(name));
-  });
+  document.querySelectorAll(`[${name}]`).forEach((el) => {
+    definition.init?.call(el, el.getAttribute(name))
+  })
 
   // Watch for future elements added dynamically
-  const observer = new MutationObserver(mutations => {
-    mutations.forEach(m => {
-      m.addedNodes.forEach(node => {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((m) => {
+      m.addedNodes.forEach((node) => {
         if (node.nodeType === 1 && node.hasAttribute(name)) {
-          definition.init?.call(node, node.getAttribute(name));
+          definition.init?.call(node, node.getAttribute(name))
         }
-      });
-    });
-  });
+      })
+    })
+  })
 
-  observer.observe(document.body, { childList: true, subtree: true });
+  observer.observe(document.body, { childList: true, subtree: true })
 }
 
 Object.entries(components).forEach(([name, definition]) => {
-    registerComponent(name, definition);
-  });
+  registerComponent(name, definition)
+})
+
+// Check if HMR is enabled
+if (module.hot) {
+  module.hot.accept() // Accept updates for this module
+}
