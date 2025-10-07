28b0ca7031ecafe9337167b6121f700423ebed58
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2025-10-06 19:52:42 -0700

    prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: false,
+  printWidth: 200,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+}
