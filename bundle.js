49690063ff12ee0655d44fef2b2bfce74758265b
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2025-10-27 12:42:25 -0700

    Updates

---
+++ bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+document.body.style.touchAction = 'none';
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQUEsUUFBUSxDQUFDQyxJQUFJLENBQUNDLEtBQUssQ0FBQ0MsV0FBVyxHQUFHLE1BQU0sQyIsInNvdXJjZXMiOlsid2VicGFjazovL2h0bWwtc2NyZWVuc2hvdC1zaGFyZS8uL3NyYy9hcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiZG9jdW1lbnQuYm9keS5zdHlsZS50b3VjaEFjdGlvbiA9ICdub25lJ1xyXG5cclxuIl0sIm5hbWVzIjpbImRvY3VtZW50IiwiYm9keSIsInN0eWxlIiwidG91Y2hBY3Rpb24iXSwic291cmNlUm9vdCI6IiJ9
