6cbd21acda07f106fc774d67c78160f6b9c45947
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-08-17 16:57:47 -0700

    add updated project

--- src/components/portfolio-generate.js
+++ src/components/portfolio-generate.js
@@ -11,7 +11,7 @@ export const PortfolioGenerate = {
   generatePortfolioSquares(preview) {
     const portfolioSquare = document.createElement('div')
     portfolioSquare.className = 'portfolio-square cantap'
-    if (!preview.title.includes('Haribo') && !preview.title.includes('American Express')) {
+    if (!preview.title.includes('Haribo') && !preview.title.includes('American Express') && !preview.title.includes('OKX')) {
       portfolioSquare.classList.remove('cantap')
       portfolioSquare.classList.add('tempUnavail')
     }
